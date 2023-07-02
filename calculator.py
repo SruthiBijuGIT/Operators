9af4e2bca4c7e93ea1c09721d647266f5b3b426c
SruthiBijuGIT <128403226+SruthiBijuGIT@users.noreply.github.com>
2023-07-02 09:25:34 -0700

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,16 @@
+#Write a python program to perform all arithmetic operations (Give Numbers Directly)
+#A. Add Two Numbers
+#B. Difference of Two Numbers
+#C. Product of Two Numbers
+#D. Quotient of Two Numbers
+#E. Remainder of Two Numbers
+#F. Exponent of a number
+x=9
+y=3
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+
