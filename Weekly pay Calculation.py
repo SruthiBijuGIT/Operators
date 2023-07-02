d69a4ee97421437f2f3def1f02371ce7224d3334
SruthiBijuGIT <128403226+SruthiBijuGIT@users.noreply.github.com>
2023-07-02 09:29:25 -0700

    Create Weekly pay Calculation.py

---
+++ Weekly pay Calculation.py
@@ -0,0 +1,7 @@
+#An employee’s total weekly pay equals the hourly wage multiplied by the total number of regular hours plus any overtime pay. Overtime pay equals the total overtime hours multiplied by 1.5 times the hourly wage. Write a program that takes as inputs the hourly wage, total regular hours, and total overtime hours and displays overtime pay and an employee’s total weekly pay.
+x=100
+y=16
+z=6
+print(int(x*z*1.5))
+print (int(x*y)+(x*z*1.5))
+
